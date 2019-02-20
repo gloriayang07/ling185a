a8181345de7d0f384827b9be978f2e4242f1d5d9
Jacob Fisher <jacobfisher18@gmail.com>
2019-02-20 01:50:56 -0800

    finished mostly

--- Assignment6/Assignment06.hs
+++ Assignment6/Assignment06.hs
@@ -2,8 +2,6 @@ module Assignment06 where
 
 import CFG
 
-import LeftmostCheck -- temp
-
 lhs :: RewriteRule nt t -> nt
 lhs r = case r of
         NontermRule x (y,z) -> x
@@ -70,13 +68,13 @@ root tree =
 --            Write all your code below this line.
 ------------------------------------------------------------------
 
--- A
--- helper function: returns whether a list of symbols contains a non-terminal symbol
+-- A | helper function: returns whether a list of symbols contains a non-terminal symbol
 listContainsNT :: [Symbol nt t] -> Bool
 listContainsNT list = case list of
                 [] -> False
                 (x:xs) -> case x of {T val -> False; NT val -> True} || listContainsNT xs
 
+-- A
 terminalsOnly :: [Symbol nt t] -> Maybe [t]
 terminalsOnly list = case listContainsNT list of
     True -> Nothing
@@ -97,23 +95,31 @@ treeToRuleList tree = case tree of
         ++ treeToRuleList rightTree
 
 -- D
--- ruleListToTree :: (Eq nt, Eq t) => [RewriteRule nt t] -> Maybe (Tree nt t)
--- ruleListToTree list startingSymbol = case list of
---     [] -> Nothing
---     (rule:rest) -> case rule of
---         TermRule mother child -> Just (Leaf mother child)
---         NontermRule mother (leftChild,rightChild) ->
---             let leftSubtree = ruleListToTree rest
+ruleListToTree :: (Eq nt, Eq t) => [RewriteRule nt t] -> Maybe (Tree nt t)
+ruleListToTree list = case leftSideOfFirstRuleInList list of
+                        Nothing -> Nothing
+                        Just startingVal -> ruleListToTreeHelper list startingVal
 
-ruleListToTree list node = case list of
+-- D | helper function
+-- Gets starting element
+leftSideOfFirstRuleInList :: (Eq nt, Eq t) => [RewriteRule nt t] -> Maybe (nt)
+leftSideOfFirstRuleInList list = case list of
+    [] -> Nothing
+    (x:xs) -> case x of
+        TermRule mother child -> Just mother
+        NontermRule mother (leftChild,rightChild) -> Just mother
+
+-- D | helper function
+-- This almost works!
+ruleListToTreeHelper list node = case list of
     [] -> Nothing
     (rule:rest) -> case rule of
         TermRule mother child ->
             if mother == node then Just (Leaf mother child)
             else Nothing
         NontermRule mother (leftChild,rightChild) ->            
-            let maybeLeftTree = ruleListToTree rest leftChild in
-            let maybeRightTree = ruleListToTree rest rightChild in
+            let maybeLeftTree = ruleListToTreeHelper rest leftChild in
+            let maybeRightTree = ruleListToTreeHelper rest rightChild in
             case maybeLeftTree of
                 Nothing -> Nothing
                 Just treeL -> case maybeRightTree of
@@ -122,72 +128,13 @@ ruleListToTree list node = case list of
                         False -> Nothing
                         True -> Just (NonLeaf mother treeL treeR)
 
--- getRuleForNode :: (Eq nt, Eq t) => [RewriteRule nt t] -> nt -> Maybe (RewriteRule nt t)
--- getRuleForNode list nonterm = case list of
---     [] -> Nothing
---     (rule:rest) -> case rule of
---         TermRule mother child ->
---             if mother == nonterm then Just rule
---             else getRuleForNode rest nonterm
---         NontermRule mother (leftChild,rightChild) ->
---             if mother == nonterm then Just rule
---             else getRuleForNode rest nonterm
-
--- test rule list
-list1 = [NontermRule "S" ("NP","VP"), TermRule "NP" "Mary", TermRule "VP" "ran"]
-list2 = [NontermRule "S" ("NP","VP"), TermRule "NP" "Mary", TermRule "VP" "ran", TermRule "NP" "Jeff"]
-list3 = [TermRule "NP" "Mary"]
-
--- -- returns a list of rules in a list starting with the given nonterminal node
--- rulesStartingWithNode :: (Eq nt, Eq t) => nt -> [RewriteRule nt t] -> [RewriteRule nt t]
--- rulesStartingWithNode node list =
---     filter (\rule -> case rule of {
---         TermRule mother child -> mother == node;
---         NontermRule mother (leftChild,rightChild) -> mother == node;
---     }) list
-
--- -- returns an array of children of the given node
--- getChild node list =
---     let relevantRules = rulesStartingWithNode node list in
---     case relevantRules of
---     [] -> []
---     (x:xs) -> case x of
---         TermRule mother child -> [child]
---         NontermRule mother (leftChild,rightChild) -> [leftChild,rightChild]
-
--- getStartingNode list =
---     case list of
---     [] -> ""
---     (x:xs) -> case x of
---         TermRule mother child -> getStartingNode xs
---         NontermRule mother (leftChild,rightChild) -> mother
-
--- node is something like T "Mary" or NT "VP"
--- recursiveThing node =
---     case node of
---         T t -> Leaf TS_MOTHER t
---         NT nt -> NonLeaf (recursiveThing THING_NONLEAF_CAN_GO_TO_1) (recursiveThing THING_NONLEAF_CAN_GO_TO_1)
-
--- motherOfTerminalNode node list = 
---     case list of
---         [] -> []
---         (rule: rest) -> case rule of
---             TermRule mother child -> case child of {node -> mother; x -> motherOfTerminalNode node rest}
---             NontermRule mother (leftChild,rightChild) -> motherOfTerminalNode node rest
-
--- childrenOfNonterminalNode
-
-        -- Rule List:   [NontermRule "S" ("NP","VP"), TermRule "NP" "Mary", TermRule "VP" "ran"]
-
--- Tree:        Just (NonLeaf "S" (Leaf "NP" "Mary") (Leaf "VP" "ran"))
-
--- E
--- helper function: get all of a tree's terminal symbols
+-- E | helper function: get all of a tree's terminal symbols
 treeToTerminals :: Tree nt t -> [Symbol nt t]
 treeToTerminals tree = case tree of
     Leaf nonterm term -> [T term]
     NonLeaf nonterm leftTree rightTree -> treeToTerminals leftTree ++ treeToTerminals rightTree
 
+-- E
 treeToDerivation :: Tree nt t -> [[Symbol nt t]]
 treeToDerivation tree = case tree of
     Leaf nonterm term -> [[NT nonterm], [T term]]
@@ -198,24 +145,7 @@ treeToDerivation tree = case tree of
         ++ [ left ++ right | left <- leftResult, right <- [[NT (root rightTree)]]]
         ++ [ left ++ right | left <- [(treeToTerminals leftTree)], right <- rightResult]
 
--- Section notes on E:
--- might need a function to get the last thing in a list (i.e. last leftResult)
--- will need to do "let leftResult = treeToDerivation SOMETHING because I'll need to use left result a couple times"
--- list of steps to process the NP
--- stick VP to the end of the steps
--- use map or list comprehension to stick VP to the result of everything
--- need to calculate the treeToDerivation steps (maybe using let) before adding anything
--- "could be cleaner to write out two versions, one that takes Leaf and one nonleaf"
--- hint: you gotta use list comprehension
--- for left: we're appending the root of the right tree
--- for right: we're sticking terminalNodes of the left tree before the right tree
-
--- Example trees for E
-tree1 = (NonLeaf "S" (Leaf "NP" "Mary") (Leaf "VP" "ran"))
-tree2 = (NonLeaf "DP" (Leaf "D" "the") (Leaf "N" "dog"))
-tree3 = (NonLeaf "VP" (Leaf "V" "ran") (Leaf "Adv" "quickly"))
-tree4 = (NonLeaf "S" tree2 tree3)
-
+-- F
 splitAtLeftmost :: (Eq nt, Eq t) => [Symbol nt t] -> Maybe ([Symbol nt t], nt, [Symbol nt t])
 splitAtLeftmost list =
     case list of
@@ -227,9 +157,7 @@ splitAtLeftmost list =
                 Nothing -> Nothing
                 Just (ys, qs ,zs) -> Just (x:ys, qs ,zs)
 
--- let Just (left, nonterm, right) = splitAtLeftmost symbolList in
--- this worked before almost
--- -- G
+-- G
 rewriteLeftmost :: (Eq nt, Eq t) => [RewriteRule nt t] -> [Symbol nt t] -> [[Symbol nt t]]
 rewriteLeftmost ruleList symbolList =
     let splitList = (splitAtLeftmost symbolList) in
@@ -245,15 +173,20 @@ rewriteLeftmost ruleList symbolList =
                             TermRule x y -> if x == nonterm then [left ++ [T y] ++ right] ++ recursiveCall
                                                     else recursiveCall
 
--- -- H
--- derivableFrom :: (Eq nt, Eq t) => [Symbol nt t] -> [RewriteRule nt t] -> Numb -> [[t]]
+-- H
+derivableFrom :: (Eq nt, Eq t) => [Symbol nt t] -> [RewriteRule nt t] -> Numb -> [[t]]
+derivableFrom symbolList ruleList n = removeTs (derivableFromHelper symbolList ruleList n)
 
+-- H | helper function
+derivableFromHelper :: (Eq nt, Eq t) => [Symbol nt t] -> [RewriteRule nt t] -> Numb -> [[Symbol nt t]]
+derivableFromHelper symbolList ruleList n = case n of
+    Z -> []
+    S n' -> let potentialStrings = rewriteLeftmost ruleList symbolList in
+            let onesWithAtLeastOneNT = filter (\x -> listContainsNT x) potentialStrings in
+            let onesWithTerminalsOnly = filter (\x -> case (listContainsNT x) of {True -> False; False -> True}) potentialStrings in
+            concat (map (\x -> derivableFromHelper x ruleList n') onesWithAtLeastOneNT) ++ onesWithTerminalsOnly
 
--- Rule List
--- [   NontermRule "S" ("NP","VP"), 
---     NontermRule "NP" ("D","N"), 
---     NontermRule "VP" ("V","NP"), 
---     TermRule "NP" "John",  TermRule "NP" "Mary", 
---     TermRule "D" "the",    TermRule "D" "a", 
---     TermRule "N" "cat",    TermRule "N" "dog", 
---     TermRule "V" "saw",    TermRule "V" "likes"]
+-- H | helper function
+-- turns things like [[T "John", T "ran"],[T "Mary"]] into [["John", "ran"],["Mary"]]
+removeTs :: (Eq nt, Eq t) => [[Symbol nt t]] -> [[t]]
+removeTs list = map (\listOfSymbols -> (map (\sy -> case sy of {T val -> val}) listOfSymbols)) list
