646488ac060fdab89785b6c8f0ab49ad9f5bba12
Jacob Fisher <jacobfisher18@gmail.com>
2019-02-24 11:40:05 -0800

    added files for assignment 7

---
+++ Assignment7/Assignment07.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Assignment07 where
+
+import Prelude hiding (init)
+
+import SemiringCFG
+
+-------------------------------------
+-- probToBool is described at the 
+-- beginning of section 2
+
+probToBool :: GenericCFG nt t Double -> GenericCFG nt t Bool
+probToBool (starts, rules) =
+    ([(c, p > 0) | (c,p) <- starts], [(r, p > 0) | (r,p) <- rules])
+
+--------------------------------
+-- setup for questions F and G
+
+data Cost = TheInt Int | Inf deriving (Show, Eq)
+
+isCompoundRule :: (Eq nt) => RewriteRule nt t -> Bool
+isCompoundRule (NontermRule c (c1,c2)) = c == c1 && c == c2
+isCompoundRule (TermRule c x) = False
+
+isAdjunctRule :: (Eq nt) => RewriteRule nt t -> Bool
+isAdjunctRule (NontermRule c (c1,c2)) = (c == c1 || c == c2) && (c1 /= c2)
+isAdjunctRule (TermRule c x) = False
+
+------------------------------------------------------------------
+------------------------------------------------------------------
+-- IMPORTANT: Please do not change anything above here.
+--            Write all your code below this line.
+
