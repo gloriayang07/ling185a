b6c826da83befb54e13fcee4804b4efaa5072eb7
Jacob Fisher <jacobfisher18@gmail.com>
2019-03-13 15:19:06 -0700

    supports more kinds of rules

--- FinalProject/FinalProject.hs
+++ FinalProject/FinalProject.hs
@@ -4,7 +4,7 @@ module FinalProject where
 
 import Assignment05
 
--- From Assignment05
+-- From Assignment #5
 type GenericAutomaton st sy v = ([(st,v)], [(st,v)], [(st,sy,v,st)])
 
 -- Potential to-do's
@@ -21,17 +21,47 @@ type GenericAutomaton st sy v = ([(st,v)], [(st,v)], [(st,sy,v,st)])
 -- Of the form (isOptional, Target, Replacement, Predecessor, Successor) in Target -> Replacement / Predecessor, Successor
 type Rule = (Bool, Char, Char, Char, Char)
 
--- Rules
-voicing = (False, 't', 'd', 'a', 'a')
-raising = (False, 'a', 'u', 'p', 'd')
+-- Can have lists of characters as environment (i.e. 'apply this rule when beween vowels'), or Nothing
+type NaturalClassRule = (Bool, Char, Char, Maybe [Char], Maybe [Char])
 
+-- Some made up rules that would probably make a phonologist very angry
+voicing = (False, 't', 'd', 'a', 'a')
+raising = (True, 'a', 'u', 'p', 'd')
+
+-- Two potential rule orderings, which will lead to different surface forms
 ruleOrderOne = [voicing, raising]
 ruleOrderTwo = [raising, voicing]
 
--- The alphabet we're working with
-phonemes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-allophones = ['A', 'T', 'B']
-alphabet = phonemes ++ allophones
+-- Some rules that accomodate natural classes
+vowels = ['a', 'e', 'i', 'o', 'u']
+voicingBetweenVowels = (False, 't', 'd', Just vowels, Just vowels)
+voicingAfterAVowel = (False, 't', 'd', Just vowels, Nothing)
+voicingBeforeAVowel = (False, 't', 'd', Nothing, Just vowels)
+
+-- Quick little helper function for getting the alphabet, excluding a given character
+alphabetWithout :: Char -> [Char]
+alphabetWithout c = [sy | sy <- alphabet, sy /= c]
+
+-- The alphabet
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+wordBoundary = ['#']
+alphabet = letters ++ wordBoundary
+
+-- From Assignment #5
+tappingFST :: GenericAutomaton Int Char [String]
+tappingFST = (  [ (0, [[]]) ],
+                [ (0, [[]]),
+                  (1, [[]]),
+                  (2, ["t"]) ],
+                [ (0, 'n', ["n"], 0),
+                  (0, 't', ["t"], 0),
+                  (0, 'a', ["a"], 1),
+                  (1, 'n', ["n"], 0),
+                  (1, 'a', ["a"], 1),
+                  (1, 't', [[]], 2),
+                  (2, 'a', ["ta", "Ta"], 1),
+                  (2, 'n', ["tn"], 0),
+                  (2, 't', ["tt"], 0)  ])
 
 -- Take a rule and return a FST that applies the rule
 ruleToFST :: Rule -> GenericAutomaton Int Char [String]
@@ -60,3 +90,26 @@ applyRuleList ruleList strList =
     case ruleList of
         [] -> strList
         (rule:rules) -> applyRuleList rules ( concat [ val (ruleToFST rule) str | str <- strList])
+
+-- Take a natural class rule and return a FST that applies the rule
+naturalClassRuleToFST :: NaturalClassRule -> GenericAutomaton Int Char [String]
+naturalClassRuleToFST rule = 
+    let (isOp, tar, rep, maybePreds, maybeSucs) = rule in
+    let preds = case maybePreds of {Nothing -> alphabetWithout tar; Just l -> l} in
+      let sucs = case maybeSucs of {Nothing -> alphabetWithout tar; Just l -> l} in
+            -- starts
+        (   [ (0, [[]]) ],
+
+            -- ends
+            [ (0, [[]]),
+              (1, [[]]),
+              (2, [[tar]]) ],
+
+            -- transitions
+            [ (1, tar, [[]], 2) ]
+              ++ [(2, suc, case isOp of {True-> [[rep, suc], [tar, suc]]; False -> [[rep, suc]]}, case (elem suc preds) of {True -> 1; False -> 0}) | suc <- sucs]
+              ++ [ (0, pred, [[pred]], 1) | pred <- preds ]
+              ++ [ (1, pred, [[pred]], 1) | pred <- preds ]
+              ++ [ (0, el, [[el]], 0) | el <- alphabet, not (elem el preds) ]
+              ++ [ (1, el, [[el]], 0) | el <- alphabet, not (elem el preds) && el /= tar ]
+              ++ [ (2, el, [[tar, el]], 0) | el <- alphabet, not (elem el sucs) ] )
