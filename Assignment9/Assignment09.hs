90b3526c5e692f20b75487fffd7e76715cc7be51
Jacob Fisher <jacobfisher18@gmail.com>
2019-03-12 20:51:58 -0700

    finished assignment9

--- Assignment9/Assignment09.hs
+++ Assignment9/Assignment09.hs
@@ -78,4 +78,52 @@ tree_13 = Binary Mrg (Leaf "Q")
 ------------------------------------------------------------------
 -- IMPORTANT: Please do not change anything above here.
 --            Write all your code below this line.
+------------------------------------------------------------------
 
+-- #1 Boolean inside values
+insideCheck :: (Ord st, Eq sy0, Eq sy1, Eq sy2) => Automaton st sy0 sy1 sy2 -> Tree sy0 sy1 sy2 -> st -> Bool
+insideCheck fsta tree st =
+  let (ends, nullaries, unaries, binaries) = fsta in
+  let allTheStates = allStates fsta in
+    case tree of
+      Leaf x ->                 elem (x, st) nullaries
+      Unary x t -> or [         (insideCheck fsta t q') &&
+                                (elem (q', x, st) unaries)
+                                | q' <- allTheStates]
+      Binary x t1 t2 -> or [    (insideCheck fsta t1 q1') &&
+                                (insideCheck fsta t2 q2') &&
+                                (elem (q1', q2', x, st) binaries) 
+                                | q1' <- allTheStates, q2' <- allTheStates]
+
+-- Possibly another (more efficient) way to do #1
+-- insideCheck :: (Ord st, Eq sy0, Eq sy1, Eq sy2) => Automaton st sy0 sy1 sy2 -> Tree sy0 sy1 sy2 -> st -> Bool
+-- insideCheck fsta tree st =
+--   let (ends, nullaries, unaries, binaries) = fsta in
+--     case tree of
+--       Leaf x -> elem st [q | (y,q) <- nullaries, y == x]
+--       Unary x t -> elem st [q | (q',y,q) <- unaries, y == x, (insideCheck fsta t q')]
+--       Binary x t1 t2 -> elem st [q | (q1,q2,y,q) <- binaries, y == x, (insideCheck fsta t1 q1), 
+--                                                                        (insideCheck fsta t2 q2)]
+
+-- #2 Wh-in-situ dependencies
+fsta_wh2 :: Automaton WhStatus String Gam1 MrgAdj
+fsta_wh2 = (-- ending states
+            [WhOK],
+            -- nullary transitions
+            [(s,WhOK) | s <- ["John","Mary","ate","bought","an","apple","books","yesterday","C","laughed","because"]] ++
+            [(s,Wh) | s <- ["WH"]] ++
+            [("Q",LicWh)],
+            -- unary transitions
+            [],
+            -- binary transitions
+            [(Wh, Wh,     Mrg, Wh),
+             (Wh, WhOK,   Mrg, Wh),
+             (WhOK, Wh,   Mrg, Wh),
+             (WhOK, WhOK, Mrg, WhOK),
+             (LicWh, Wh,  Mrg, WhOK),
+             (Wh, LicWh,  Mrg, WhOK),
+             
+             (WhOK, WhOK, Adj, WhOK),
+             (LicWh, Wh,  Adj, WhOK),
+             (Wh, LicWh,  Adj, WhOK)
+             ])
